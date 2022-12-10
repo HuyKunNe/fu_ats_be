5901e53ce78ce676ef60e895846abf36e04746b4
Huy Kun <dinhquanghuydt@gmail.com>
2022-12-11 00:07:30 +0700

    imp plan detail

--- src/main/java/com/fu/fuatsbe/repository/PlanDetailRepository.java
+++ src/main/java/com/fu/fuatsbe/repository/PlanDetailRepository.java
@@ -19,27 +19,30 @@ import com.fu.fuatsbe.entity.RecruitmentPlan;
 @Transactional
 @Repository
 public interface PlanDetailRepository extends JpaRepository<PlanDetail, Integer> {
-    Optional<PlanDetail> findById(int id);
+        Optional<PlanDetail> findById(int id);
 
-    Page<PlanDetail> findByStatus(String status, Pageable pageable);
+        Page<PlanDetail> findByStatus(String status, Pageable pageable);
 
-    Page<PlanDetail> findByRecruitmentPlan(RecruitmentPlan recruitmentPlan, Pageable pageable);
+        Page<PlanDetail> findByRecruitmentPlan(RecruitmentPlan recruitmentPlan, Pageable pageable);
 
-    Page<PlanDetail> findByApprover(Employee employee, Pageable pageable);
+        Page<PlanDetail> findByApprover(Employee employee, Pageable pageable);
 
-    @Query(value = "select coalesce(sum(p.amount),0) as totalAmount from plan_detail p \n"
-            + " join recruitment_plan r on p.recruitment_plan_id = r.id \n"
-            + " where p.status like '%APPROVED%' and r.id = ?1", nativeQuery = true)
-    Integer totalAmount(int id);
+        @Query(value = "select coalesce(sum(p.amount),0) as totalAmount from plan_detail p \n"
+                        + " join recruitment_plan r on p.recruitment_plan_id = r.id \n"
+                        + " where p.status like '%APPROVED%' and r.id = ?1", nativeQuery = true)
+        Integer totalAmount(int id);
 
-    @Query(nativeQuery = true, value = "select * from plan_detail where creator_id " +
-            "in(select id from employee where department_id = ?1) and status like 'APPROVED';")
-    List<PlanDetail> findApprovedByDepartment(int departmentId);
+        @Query(nativeQuery = true, value = "select * from plan_detail where creator_id " +
+                        "in(select id from employee where department_id = ?1) and status like 'APPROVED';")
+        List<PlanDetail> findApprovedByDepartment(int departmentId);
 
-    @Query(nativeQuery = true, value = "(select coalesce(status, 'PENDING')  as status, count(status) as total from plan_detail where status like 'PENDING') " +
-            "union (select  coalesce(status, 'APPROVED') , count(status)  from plan_detail where status like 'APPROVED') " +
-            "union( select  coalesce(status, 'CANCELED') ,count(status) from plan_detail where status like'CANCELED')")
-    List<Tuple> getTotalStatusDetail();
-    @Query(nativeQuery = true, value = "select * from plan_detail where creator_id in(select id from employee where department_id = ?1) order by date desc")
-    Page<PlanDetail> getPlanDetailByDepartment(int departmentId, Pageable pageable);
+        @Query(nativeQuery = true, value = "(select coalesce(status, 'PENDING')  as status, count(status) as total from plan_detail where status like 'PENDING') "
+                        +
+                        "union (select  coalesce(status, 'APPROVED') , count(status)  from plan_detail where status like 'APPROVED') "
+                        +
+                        "union( select  coalesce(status, 'REJECTED') ,count(status) from plan_detail where status like'REJECTED')")
+        List<Tuple> getTotalStatusDetail();
+
+        @Query(nativeQuery = true, value = "select * from plan_detail where creator_id in(select id from employee where department_id = ?1) order by date desc")
+        Page<PlanDetail> getPlanDetailByDepartment(int departmentId, Pageable pageable);
 }
